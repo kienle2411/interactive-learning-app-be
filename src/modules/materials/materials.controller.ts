fac47e18538f9c188b45c1f30c1b9830bdbd7d12
Le Trung Kien <22520705@gm.uit.edu.vn>
2024-11-18 23:39:09 +0700

    feature: material-route

--- src/modules/materials/materials.controller.ts
+++ src/modules/materials/materials.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
+import { UpdateMaterialDto } from './dto/update-material.dto';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { RolesGuard } from '../auth/guard/roles.guard';
+import { Roles } from '../auth/decorator/roles.decorator';
+import { MaterialsService } from './materials.service';
 
 @Controller('materials')
-export class MaterialsController {}
+export class MaterialsController {
+  constructor(private readonly materialsService: MaterialsService) {}
+
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('teacher')
+  async updateMaterialInformation(
+    @Param() materialId: string,
+    @Body() updateMaterialDto: UpdateMaterialDto,
+  ) {
+    return await this.materialsService.updateMaterialInformation(
+      materialId,
+      updateMaterialDto,
+    );
+  }
+
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  async getMaterialInformation(@Param() materialId: string) {
+    return await this.materialsService.getMaterialInformation(materialId);
+  }
+}
--- src/modules/materials/materials.service.ts
+++ src/modules/materials/materials.service.ts
@@ -1,4 +1,26 @@
+import { PrismaService } from '@/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { UpdateMaterialDto } from './dto/update-material.dto';
 
 @Injectable()
-export class MaterialsService {}
+export class MaterialsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getMaterialInformation(materialId: string) {
+    return this.prisma.material.findUnique({
+      where: { id: materialId },
+    });
+  }
+
+  async updateMaterialInformation(
+    materialId: string,
+    updateMaterialDto: UpdateMaterialDto,
+  ) {
+    return this.prisma.material.update({
+      data: {
+        ...updateMaterialDto,
+      },
+      where: { id: materialId },
+    });
+  }
+}
